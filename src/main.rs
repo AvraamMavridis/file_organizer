bbcd00b090601db3a5f5da770f10cd29dbefdb7b
AvraamMavridis <avraammauridis@gmail.com>
2024-05-03 08:31:06 +0300

    refactor: simplify

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+mod categories;
+
 use std::collections::HashMap;
-use std::fs;
 use std::env;
+use std::fs;
 use std::path::Path;
+use crate::categories::CATEGORIES;
 
 type DirEntries = Vec<fs::DirEntry>;
+type Categories = HashMap<String, Vec<String>>;
 
 fn list_files_in_directory(path: &Path) -> DirEntries {
     let mut files = vec![];
@@ -17,17 +21,55 @@ fn list_files_in_directory(path: &Path) -> DirEntries {
     files
 }
 
-fn categorize_files(files: DirEntries) -> HashMap<String, DirEntries> {
-    let mut categories = HashMap::new();
+fn get_category(ext: &str) -> Option<&'static str> {
+    CATEGORIES.get(ext).cloned()
+}
+
+fn categorize_files(files: DirEntries) -> HashMap<String, Vec<String>> {
+    let mut categories: HashMap<String, Vec<String>> = HashMap::new();
     for file in files {
-        let filetype: String = file.path().extension().unwrap_or_default().to_string_lossy().to_lowercase();
-        if filetype != "" {
-            categories.entry(filetype).or_insert_with(Vec::new).push(file);
+        let filetype: String = file
+            .path()
+            .extension()
+            .unwrap_or_default()
+            .to_string_lossy()
+            .to_lowercase();
+        let category = get_category(&filetype);
+        if category.is_some() {
+            categories
+                .entry(category.unwrap().to_string())
+                .or_insert_with(Vec::new)
+                .push(file.path().to_string_lossy().into_owned());
         }
     }
 
     categories
 }
+
+fn move_files(path: &str, category_files: &[String]) {
+    let new_dir_path = Path::new(&path);
+
+    for file in category_files {
+        let old_path = Path::new(file);
+        let new_path = new_dir_path.join(old_path.file_name().unwrap());
+        match fs::rename(&old_path, &new_path) {
+            Ok(_) => println!("Successfully moved file to {}", new_path.display()),
+            Err(e) => println!("Failed to move file: {}", e),
+        }
+    }
+}
+
+fn handle_category(path: &str, cat: &str, category_files: &[String]) {
+    let new_dir_path = Path::new(path).join(cat);
+    if !new_dir_path.exists() {
+        if let Err(e) = fs::create_dir(&new_dir_path) {
+            println!("Failed to create directory: {}, error: {}", cat, e);
+            return;
+        }
+    }
+    move_files(path, category_files);
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -35,12 +77,12 @@ fn main() {
         println!("Please provide a directory path");
         return;
     }
-    let path = Path::new(&args[1]);
-    let files = list_files_in_directory(path);
+    let path = &args[1];
+    let files = list_files_in_directory(Path::new(path));
 
-    let categories = categorize_files(files);
+    let categories: Categories = categorize_files(files);
 
-    for cat in categories {
-        println!("{}", cat.0)
+    for (cat, files) in &categories {
+        handle_category(path, &cat, files);
     }
 }
